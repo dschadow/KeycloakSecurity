15f4c65f9acecf5613bcc74dda712f1d10694c30
Dominik Schadow <mail@dominikschadow.de>
2023-01-03 10:18:17 +0100

    refactored package

--- Backend/src/test/java/de/dominikschadow/keycloaksecurity/ApplicationTests.java
+++ Backend/src/test/java/de/dominikschadow/keycloaksecurity/ApplicationTests.java
@@ -1,10 +1,10 @@
-package de.dominikschadow.keycloaksecurity.backend;
+package de.dominikschadow.keycloaksecurity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
