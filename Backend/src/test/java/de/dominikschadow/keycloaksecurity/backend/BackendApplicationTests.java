85730fcc8056374afd6eb663bfe431eed597f12f
Dominik Schadow <mail@dominikschadow.de>
2023-01-03 09:44:17 +0100

    initial commit

---
+++ Backend/src/test/java/de/dominikschadow/keycloaksecurity/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dominikschadow.keycloaksecurity.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
