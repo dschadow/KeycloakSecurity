a6b346fdf30a73b32e84b5c27fb8758400bdbc25
Dominik Schadow <mail@dominikschadow.de>
2023-01-14 13:07:36 +0100

    refactoring, match new API

--- Backend/src/main/java/de/dominikschadow/duke_encounters/encounter/EncounterController.java
+++ Backend/src/main/java/de/dominikschadow/duke_encounters/encounter/EncounterController.java
@@ -1,16 +1,33 @@
 package de.dominikschadow.duke_encounters.encounter;
 
 import de.dominikschadow.duke_encounters.api.EncounterApi;
+import de.dominikschadow.duke_encounters.model.CreateEncounter;
 import de.dominikschadow.duke_encounters.model.Encounter;
+import de.dominikschadow.duke_encounters.model.EncounterList;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class EncounterController implements EncounterApi {
     @Override
-    public ResponseEntity<List<Encounter>> readEncounters() {
+    public ResponseEntity<Encounter> createEncounter(CreateEncounter createEncounter) {
+        return EncounterApi.super.createEncounter(createEncounter);
+    }
+
+    @Override
+    public ResponseEntity<Encounter> readEncounter(UUID encounterId) {
+        return EncounterApi.super.readEncounter(encounterId);
+    }
+
+    @Override
+    public ResponseEntity<EncounterList> readEncounters() {
         return EncounterApi.super.readEncounters();
+    }
+
+    @Override
+    public ResponseEntity<EncounterList> readLatestEncounters() {
+        return EncounterApi.super.readLatestEncounters();
     }
 }
